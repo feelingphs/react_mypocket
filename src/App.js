9e5f65133dd5b17dd6edc4ea83258c51e454a3db
박혜선 <hspark@vinylc.com>
2024-06-21 17:54:04 +0900

    가계부 ui 작업중

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 //import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
-import './App.scss';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+import './App.scss';
+
 function App() {
-  const date = ["1일", "2일", "3일", "4일", "5일", "6일", "7일", "8일"]
-  // const category = ["식비", "교통", "공과금"]
+  const date = ["1", "2", "3", "4", "5", "6", "7", "28"]
+  const week = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+  const type = ["expenses", "expenses", "expenses", "income"]
+  const itemName = ["GS편의점 도시락", "야간 택시", "전기세", "월급"]
+  const itemCategory = ["식비", "교통", "공과금", "월급"]
+  const itemPrice = ["- 5,900", "- 9,000", "- 59,000", "+ 400,000"]
   return (
     <div className="wrap">
       <main className="main" id="main">
@@ -25,37 +30,71 @@ function App() {
           <div className="">
             <Swiper 
               className="mySwiper"
-              slidesPerView={5.5} 
-              spaceBetween={10}
+              slidesPerView={7.5} 
+              spaceBetween={16}
               modules={[Navigation]}
             >
               {
-                date.map((i) => {
+                date.map((date, i) => {
                   return (
-                    <SwiperSlide><button>{i}</button></SwiperSlide>
+                    <SwiperSlide>
+                      <button className="pocket_date">
+                        <span className="date">{ date }</span>
+                        <span className="week">{ week[i] }</span>
+                      </button>
+                    </SwiperSlide>
                   )
-                }
-                )
+                })
               }
             </Swiper>
             <div className="pocket_list">
-              <button className="pocket_item">
-                <div className="item_info">
-                  <span className="item_name">GS25 우유</span>
-                  <span className="item_category">식비</span>
-                </div>
-                <div className="item_price">-2,000원</div>
-              </button>
-              <button className="pocket_item">
-                <div className="item_info">
-                  <span className="item_name">GS25 우유</span>
-                  <span className="item_category">식비</span>
-                </div>
-                <div className="item_price">-2,000원</div>
-              </button>
+              {
+                itemName.map((itemName, i) => {
+                  return(
+                    <button className={ "pocket_item " + type[i] }>
+                      <div className="item_info">
+                        <span className="item_name">{ itemName }</span>
+                        <span className="item_category">{ itemCategory[i] }</span>
+                      </div>
+                      <div className="item_price">{ itemPrice[i] }원</div>
+                    </button>
+                  )
+                })
+              }
               <button className="pocket_add">
-                추가
+                <span>+</span>
               </button>
+            </div>
+            <div className="layer_wrap">
+              <div className="layer_type">
+                <div className="type_wrap">
+                  <button className="type_expenses">
+                    <span>지출</span>
+                  </button>
+                  <button className="type_income">
+                    <span>수입</span>
+                  </button>
+                </div>
+                <div className="dropdown_wrap">
+                  <label for="category">카테고리</label>
+                  <select id="category">
+                    <option value="food">식비</option>
+                    <option value="transortation">교통</option>
+                    <option value="dues">공과금</option>
+                  </select>
+                </div>
+                <div className="input_wrap">
+                  <label for="input_item_name">내역</label>
+                  <input type="text" className="input_item_name" id="input_item_name" placeholder="장보기"></input>
+                </div>
+                <div className="input_wrap">
+                  <label for="input_item_price">금액</label>
+                  <input type="number" className="input_item_price" id="input_item_price" placeholder="30,000"></input>
+                </div>
+                <div className="button_wrap">
+                  <button className="button"><span>확인</span></button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
